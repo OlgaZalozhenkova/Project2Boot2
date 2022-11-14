6752ac087d2b81f2f9719b7b4683300b690c2112
OlgaZalozhenkova <olga.zalogenkova@mail.ru>
2022-11-14 19:41:12 +0300

    Initial commit

---
+++ src/main/java/olga/zalogenkova/Project2Boot/models/Book.java
@@ -0,0 +1,105 @@
+package olga.zalogenkova.Project2Boot.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Book {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    @NotEmpty(message = "Title should not be empty")
+    @Size(min = 2, max = 200, message = "Title should be between 1 and 200 characters")
+    private String title;
+
+    @Column(name = "author")
+    @NotEmpty(message = "Author should not be empty")
+    @Size(min = 2, max = 100, message = "Author name should be between 2 and 100 characters")
+    private String author;
+
+    @Min(value = 1500, message = "Year should be greater than 1500")
+    @Column(name = "year")
+    private int year;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    @Column(name = "taken_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date takenAt;
+
+    @Transient
+    private boolean expired; // Hibernate не будет замечать этого поля, что нам и нужно. По-умолчанию false.
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public Date getTakenAt() {
+        return takenAt;
+    }
+
+    public void setTakenAt(Date takenAt) {
+        this.takenAt = takenAt;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
+}
---
+++ src/main/java/olga/zalogenkova/Project2Boot/models/Person.java
@@ -0,0 +1,70 @@
+package olga.zalogenkova.Project2Boot.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "full_name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 1, max = 30, message = "Name should be between 1 and 100 characters")
+    private String fullName;
+
+    @Column(name = "year_of_birth")
+    @Min(value = 1900, message = "Year of birth should be greater than 1900")
+    @Max(value = 2008, message = "Reader should be older than 14 years")
+    private int yearOfBirth;
+
+    @OneToMany(mappedBy = "owner")
+    List<Book> books;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
